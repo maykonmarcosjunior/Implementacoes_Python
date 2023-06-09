d938977c620013598fc4f555fd8811a97db1f2e3
maykonmarcosjunior <112183802+maykonmarcosjunior@users.noreply.github.com>
2023-06-09 14:08:07 -0300

    Create Euclides.py

---
+++ Euclides.py
@@ -0,0 +1,23 @@
+def fibonacci(n):
+    raiz_de_5 = 5**(0.5)
+    u = 1/raiz_de_5
+    x1 = (1 + raiz_de_5)/2
+    x2 = (1 - raiz_de_5)/2
+    x1 = x1**n
+    x2 = x2**n
+    v = x1 - x2
+    u = u*v
+    return int(u)
+
+print(fibonacci(2))
+
+def euclides(a, b):
+    while b != 0:
+        a, b = b, a%b
+    return a
+def euclides_estendido(a, b):
+    if b == 0:
+        return a, 0, 1
+    else:
+        mdc, x, y = euclides_estendido(b, a%b)
+        return mdc, y-(a//b)*x, x
