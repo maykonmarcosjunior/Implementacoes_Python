d336f72289ce2ef06c48d56c7c0194c145ce20e3
maykonmarcosjunior <maykon.marcos.junior.@grad.ufsc.br>
2023-08-22 10:10:25 -0300

    otimizações

--- Isolados/URI/URI_A/URI_2392.py
+++ Isolados/URI/URI_A/URI_2392.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
+from Isolados.URI.URI_A.URI_1018 import saida
 
 Npedras, Nsapos = [int(i) for i in input().split()]
-saida = [0]*(Npedras + 1)
+saida = [""]*(Npedras + 1)
 for i in range(Nsapos):
     pedraI, pulo = [int(i) for i in input().split()]
-    saida[pedraI] = 1
+    saida[pedraI] = "1\n"
     for j in range(pedraI, Npedras+1, pulo):
-        saida[j] = 1
+        saida[j] = "1\n"
     for j in range(pedraI, 0, -pulo):
-        saida[j] = 1
-for i in range(1, Npedras+1):
-    print(saida[i])
+        saida[j] = "1\n"
+print("".join(saida), end='')
--- Isolados/URI/URI_A/URI_2423.py
+++ Isolados/URI/URI_A/URI_2423.py
@@ -4,7 +4,7 @@ def min(A, B, C):
     cond2 = int(B <= A and B <= C)
     cond3 = int(C <= A and C <= B)
     saida = [A, B, C]
-    indice = (3*cond1 + cond2 + 2*cond3 - cond2*cond3) % 3
+    indice = (3*cond1 + cond2 + 2*cond3 - (cond2 and cond3)) % 3
     return saida[indice]
 
 
