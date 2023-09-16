4f77c95e8a61a33a01d0063213b5f5c52c8f4662
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2023-09-16 11:58:50 -0300

    Transferindo para URI_Python

--- Isolados/Haskell/AtividadeVIII/principal.hs
+++
@@ -1,5 +0,0 @@
-import Pilha (Pilha (Stack), emptyStack, push, pop, top)
-
-main = do putStrLn (show (push (push emptyStack 1) 2))
-          putStrLn (show (top (Stack [1,2,3,4,5])))
-          putStrLn (show (pop (Stack [1,2,3,4,5])))
--- Isolados/URI/URI_A/URI_1387.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-saida = ""
-while True:
-    e, r = input().split()
-    if e == r == '0':
-        break
-    saida += str(int(e) + int(r))
-print(saida)
